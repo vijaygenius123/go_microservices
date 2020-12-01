a26d90c92fe9c9adbf181c2560e2f007e5cf0e5b
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-01 19:55:33 +0530

    Added Simple Log

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "Hello Visitor")
+		log.Println("Got A Visitor")
+		fmt.Fprintf(w, "Hello Visitor")
 	})
 
 	http.ListenAndServe(":8080", nil)
