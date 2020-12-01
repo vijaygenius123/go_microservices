47c295ad157f377f9686c2246424ec533e699e89
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-01 20:00:27 +0530

    Able To Get Data From Body

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got A Visitor")
-		fmt.Fprintf(w, "Hello Visitor")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Request")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte("Oops"))
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s", d)
+
 	})
 
 	http.ListenAndServe(":8080", nil)
