c4a806ad7629f4bf485e36a51554213836e9518c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-02 08:22:54 +0530

    Added Goodbye Handler

---
+++ handlers/goodbye.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodBye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (g *Goodbye) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Goodbye")
+}
--- main.go
+++ main.go
@@ -12,9 +12,11 @@ func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/bye", gh)
 
 	http.ListenAndServe(":8080", sm)
 }
